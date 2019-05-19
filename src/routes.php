81fa8b3bff2fa0649d1880387e1f3d329c632c52
Diego Henicka <diego.henicka@gmail.com>
2019-05-19 08:21:20 -0300

    Chamado de uber e integração com maps

--- src/routes.php
+++ src/routes.php
@@ -30,7 +30,73 @@ return function (App $app) {
 
     $app->get('/home', function (Request $request, Response $response, array $args) {
         return $this->renderer->render($response, 'home.phtml', $args);
-    });
+    })->add($auth);
+
+    $app->get('/localizacao', function (Request $request, Response $response, array $args) {
+        return $this->renderer->render($response, 'localizacao.phtml', $args);
+    })->add($auth);
+
+
+    $app->get('/uber', function (Request $request, Response $response, array $args) {
+        $url      = "http://localhost:8181/uber/";
+        $ClientID = "rFgw8cBkS7N-5gkE3Op6QoaV6bpnx55q";
+        $redirecionamento = 'https://login.uber.com/oauth/v2/authorize?response_type=code&client_id='.$ClientID.'&scope=all_trips+delivery+history+history_lite+places+profile+request+request_receipt+ride_widgets&redirect_uri='.$url;
+        //return $this->renderer->render($response, 'uber.phtml', $args);
+        return $response->withStatus(302)->withHeader('Location', $redirecionamento);
+        //var_dump($redirecionamento);
+
+
+    })->add($auth);
+
+    $app->get('/uber/{params:.*}', function (Request $request, Response $response, array $args) {
+
+        //var_dump($_GET['code']);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://login.uber.com/oauth/v2/token',
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => [
+                client_secret => "S3pUivMp9ijIJMNvKH-Z-LMxWd8s2qd0P5vzhhv-",
+                client_id=>"rFgw8cBkS7N-5gkE3Op6QoaV6bpnx55q",
+                grant_type=>"authorization_code",
+                redirect_uri=>"http://localhost:8181/uber/",
+                code=>$_GET['code']
+            ]
+        ]);
+        
+        $res = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            //var_dump($response);
+            $res = json_decode($res, true);
+
+            //$access_token = $_GET['code'];
+            $client = new Stevenmaguire\Uber\Client(array(
+                'access_token' => $res["access_token"],
+                'server_token' => 'HJZf1PAWDYzKJOxpW_mase8xqiABRghVtkTABplo',
+                'use_sandbox'  => true, // optional, default false           
+                'version'      => 'v1.2' 
+            ));
+
+            $products = $client->getProducts(array(
+                'latitude' => '-30.0661275',
+                'longitude' => '-51.1829802'
+            ));
+            $dados = [produtos => $products];
+            //var_dump($dados);
+            return $this->renderer->render($response, 'uber.phtml', $dados);
+        }
+
+    })->add($auth);
+
 
     $app->post('/cadastro', function (Request $request, Response $response, array $args) {
         $data = [
@@ -66,4 +132,8 @@ return function (App $app) {
           }
           return $response->withStatus(302)->withHeader('Location', '/');
     });
+
+
+
+
 };
